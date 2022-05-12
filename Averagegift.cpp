0e120a888eccf0c746e6cb08035d4eab35b7f785
Akash-Gottimukkala <76110994+Akash-Gottimukkala@users.noreply.github.com>
2022-05-12 12:09:42 +0530

    Add files via upload

---
+++ Averagegift.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World!";
+}
+#include<bits/stdc++.h>
+#include<sstream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define mt              make_tuple
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define range(a,b)		substr(a,b-a+1)
+#define w(x)            int x; cin>>x; while(x--)
+#define trace(x) 		cerr<<#x<<": "<<x<<" "<<endl;
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+void akash()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+
+
+int32_t main() {
+
+	akash();
+	w(t) {
+		int n,b;
+		cin>>n>>b;
+		vector<int> a(n);
+		for(int &x:a){
+			cin>>x;
+		}
+		sort(a.begin(),a.end());
+		if(b<a[0] || b>a[n-1]){
+			cout<<"NO"<<endl;
+		}
+		else{
+			cout<<"YES"<<endl;
+		}
+
+
+		
+
+	}
+}
